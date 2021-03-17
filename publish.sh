e80871c948fc29b4cda1ee7a48e4ded3f88fb03d
Erik Agsjö <erik.agsjo@gmail.com>
2021-03-17 21:09:29 +0100

    Deploy script fix

--- publish.sh
+++ publish.sh
@@ -4,6 +4,7 @@ git branch -f gh-deploy
 git checkout gh-deploy
 git add -f prod
 git commit -m "Deploying"
+git branch -D gh-pages
 git subtree split --prefix prod -b gh-pages
 git push -f origin gh-pages:gh-pages
 
